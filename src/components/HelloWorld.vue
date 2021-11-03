8c9d076905928f2a1347257dbee253ce458f1970
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2021-11-03 09:53:14 -0300

    feat(components): add lorem ipsum

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, quasi animi esse perspiciatis 
+      aliquam odit? Magni itaque quaerat id consequatur saepe illum libero eveniet similique. Reprehenderit placeat aperiam architecto corporis!
+    </p>
   </div>
 </template>
 
