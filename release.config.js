7602880bc477a97fbca2d82e025afb2030f2e6ec
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2022-05-10 17:04:27 -0300

    feat: remove unnecessary deps and update changelog preset

--- release.config.js
+++ release.config.js
@@ -23,7 +23,7 @@ module.exports = {
         ],
     }],
     ['@semantic-release/release-notes-generator', {
-      preset: 'angular',
+      preset: 'conventionalcommits',
       presetConfig: {
         types: [
           {
